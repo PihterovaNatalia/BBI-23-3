3d2c062f4f9686124c0747595c0151800c7b7b2b
PihterovaNatalia <159529985+PihterovaNatalia@users.noreply.github.com>
2024-04-26 13:53:51 +0300

    Add files via upload

---
+++ cv2.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
+
+class Program
+{
+    static void Task1()
+    {
+        string read_s = Console.ReadLine();
+        string s = read_s.ToLower();
+        int[] stat = new int[256 * 256];
+        for (int i = 0; i < stat.Length; i++)
+        {
+            stat[i] = 0;
+        }
+        for (int i = 0; i < s.Length; i++)
+        {
+            stat[(int)s[i]] += 1;
+        }
+        char max_c = s[0];
+        int max_val = stat[max_c];
+        for (int i = 1; i < s.Length; i++)
+        {
+            if (max_val < stat[(int)s[i]])
+            {
+                max_val = stat[(int)s[i]];
+                max_c = s[i];
+            }
+        }
+
+        Console.WriteLine("В слове '{0}' самая частая буква '{1}'. Кол-во повторений: {2}", read_s, max_c, max_val);
+    }
+
+    static string Task2(string s)
+    {
+        
+        string res = "";
+        int diff = 1071 - 1040 + 1;
+        for (int i = 0; i < s.Length; i++)
+        {
+            int code = (int)s[i];
+            if ((code < 1040) || (code > 1103))
+            {
+                res += s[i];
+                continue;
+            }
+            if (code >= 1072) 
+            {
+                code += 10;
+                if (code > 1103)
+                {
+                    code -= diff;
+                }
+            } else {  
+            
+                code += 10;
+                if (code > 1071)
+                {
+                    code -= diff;
+                }
+            }
+            res += (char)code;
+        }
+        Console.WriteLine(res);
+        return res;
+    }
+
+    static void Task3()
+    {
+        string dirName = "R:\\Solution"; 
+            Directory.CreateDirectory(dirName);
+
+        if (!File.Exists(dirName + "\\cw2_1.json")) 
+            File.Create(dirName + "\\cw2_1.json");
+
+        if (!File.Exists(dirName + "\\cw2_2.json"))
+            File.Create(dirName + "\\cw2_2.json");
+    }
+
+    static void Task4()
+    {
+        string dirName = "R:\\Solution"; 
+        if (!Directory.Exists(dirName))
+            Directory.CreateDirectory(dirName);
+        if (File.Exists(dirName + "\\cw2_1.json"))
+        {
+            
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Task3();
+    }
+}
